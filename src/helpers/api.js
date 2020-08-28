2be33469235241bc5ed13acbfab9b802bf5a2c24
Sogolon <sogolonmd@gmail.com>
2020-08-28 04:10:09 -0300

    helpers to token time

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { getToken } from './account';
+import { secondsToReadableTime } from './datetime';
+import { getTokenExpire } from './jwt';
 
 export const getApiUrl = (path) => {
     return `http://localhost:3001${path}`;
@@ -7,15 +9,24 @@ export const getApiUrl = (path) => {
 
 export const getHeaders = () => {
     const token = getToken();
+
+    // console.log('*** getHeaders.token', token);
     if(!token) return {};
+
+    const expires = getTokenExpire(token);
+
+    const secondsToExpire = expires -(Date.now() / 1000);
+    const readableTime = secondsToReadableTime(secondsToExpire);
+
+    console.log('*** getHeaders.readableTime', readableTime);
 
     return {
         Authorization: `Bearer ${token}`
-    }
+    };
 };
 
 export const apiPost = (path, data = {}) => {
-    const url =getApiUrl(path);
+    const url = getApiUrl(path);
     const options = {
         headers: getHeaders(),
     };
@@ -23,7 +34,7 @@ export const apiPost = (path, data = {}) => {
 };
 
 export const apiPut = (path, data = {}) => {
-    const url =getApiUrl(path);
+    const url = getApiUrl(path);
     const options = {
         headers: getHeaders(),
     };
@@ -31,7 +42,7 @@ export const apiPut = (path, data = {}) => {
 }; 
 
 export const apiDelete = (path, data = {}) => {
-    const url =getApiUrl(path);
+    const url = getApiUrl(path);
     const options = {
         headers: getHeaders(),
     };
@@ -39,7 +50,7 @@ export const apiDelete = (path, data = {}) => {
 }; 
 
 export const apiGet = (path) => {
-    const url =getApiUrl(path);
+    const url = getApiUrl(path);
     const options = {
         headers: getHeaders(),
     };
